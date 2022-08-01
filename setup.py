3b5bee8479b200f6c5b9f2681fcad1ac612b62ba
Daniel Vogler <daniel.vogler@mgb.ch>
2022-08-01 11:15:31 +0200

    setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.0.1"
+
+setup(name='dataclassification',
+      description='Example data classification',
+      author='Daniel Vogler',
+      packages=find_packages(),
+      url='https://github.com/danielvogler/data_classification_example.git',
+      version=str(__version__))
