39542abc4b8466551a467f2c1ada27efceb755e4
Daniel Vogler <daniel.vogler@mgb.ch>
2022-08-02 07:57:36 +0200

    streamline DB

--- dashboard/dashboard_main.py
+++ dashboard/dashboard_main.py
@@ -1,14 +1,15 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 import logging
-from dataclassification.analytics import Analytics
 from dataclassification.config import config
 from dataclassification.settings import PROJECT_ROOT
-from dataclassification.plotting import Plotting
-from dataclassification.utils import Utils
+
 
 st.set_page_config(layout="wide")
+
+import data
+import models
+import cost
+import summary
 
 ### config
 config_file = PROJECT_ROOT + '/config/config.ini'
@@ -16,21 +17,7 @@ config.initialize_config_vars( config_file )
 logging.basicConfig(encoding='utf-8', level=logging.INFO)
 logging.info(f'PROJECT_ROOT: {PROJECT_ROOT}')
 
-### init
-ana = Analytics(config)
-plts = Plotting(config)
-utils = Utils(config)
-
-
 st.title('Data classification')
-
-#app.py
-import data
-import models
-import cost
-import summary
-
-
 PAGES = {
     "Data": data,
     "Models": models,
@@ -40,7 +27,7 @@ PAGES = {
 st.sidebar.title('Navigation')
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 page = PAGES[selection]
-page.app()
+page.app(config)
 
 
 
--- dashboard/data.py
+++ dashboard/data.py
@@ -1,28 +1,15 @@
 import streamlit as st
 import logging
 import pandas as pd
-import numpy as np
-import pickle
 from pathlib import Path
-from dataclassification.analytics import Analytics
-from dataclassification.config import config
-from dataclassification.settings import PROJECT_ROOT
-from dataclassification.plotting import Plotting
 from dataclassification.utils import Utils
+from PIL import Image
 
-
-### config
-config_file = PROJECT_ROOT + '/config/config.ini'
-config.initialize_config_vars( config_file )
 logging.basicConfig(encoding='utf-8', level=logging.INFO)
-logging.info(f'PROJECT_ROOT: {PROJECT_ROOT}')
 
-### init
-ana = Analytics(config)
-plts = Plotting(config)
-utils = Utils(config)
+def app(config):
 
-def app():
+    utils = Utils(config)
 
     st.cache
 
@@ -38,14 +25,10 @@ def app():
     st.dataframe(df_data.describe())
 
     st.write('Pairplot')
-    with open(Path( config.fig_dir / 'feature_pairplot.pkl') , 'rb') as f:
-        fig = pickle.load( f )
-    st.pyplot(fig, use_container_width=True)
+    st.image( Image.open( Path(config.fig_dir / 'feature_pairplot.png') ) )
 
     st.write('Heatmap')
-    with open(Path( config.fig_dir / 'feature_heatmap.pkl') , 'rb') as f:
-        fig = pickle.load( f )
-    st.pyplot(fig, use_container_width=True)
+    st.image( Image.open( Path(config.fig_dir / 'feature_heatmap.png') ) )
 
     ###
     st.subheader('Removed data')
